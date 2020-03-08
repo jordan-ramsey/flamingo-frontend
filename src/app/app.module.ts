555e4736b26975abc39f5e89aaa8ebda8c6ac356
JordanRamsey <jrams1414@gmail.com>
2020-03-08 01:59:41 -0500

    Revert "create login component"
    This reverts commit 3d993a9973e8660ef851abce07759527ba53d5b7.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,16 @@
-import { LoginComponent } from './components/login/login.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatCardModule, MatInputModule, MatButtonModule } from '@angular/material';
-import {MatIconModule} from '@angular/material/icon';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule  
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
