3d993a9973e8660ef851abce07759527ba53d5b7
JordanRamsey <jrams1414@gmail.com>
2020-03-08 01:48:51 -0500

    create login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { LoginComponent } from './components/login/login.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule, MatInputModule, MatButtonModule } from '@angular/material';
+import {MatIconModule} from '@angular/material/icon';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
