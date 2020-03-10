628095a2506987ae9b8f904649b760512e18f137
JordanRamsey <jrams1414@gmail.com>
2020-03-10 02:30:21 -0400

    add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,37 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule, MatButtonModule, MatInputModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes),
     BrowserModule,
     AppRoutingModule,
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    BrowserAnimationsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
